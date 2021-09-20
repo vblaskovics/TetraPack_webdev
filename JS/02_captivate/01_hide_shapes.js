6524bd0968c3309dfe472195b472b72c3bce67cc
vblaskovics <viktor.blaskovics@gmail.com>
2021-09-20 15:22:12 +0200

    Add cp

---
+++ JS/02_captivate/01_hide_shapes.js
@@ -0,0 +1,13 @@
+for (let i = 1; i <= 12; i++) {
+    cp.hide("shape_" + i);
+}
+
+
+// if ( i < 10 ){
+//     cp.hide("shape_0" + i);
+// } else {
+//     cp.hide("shape_" + i);
+// }
+
+
+// cp.hide("shape_" + (i < 10) ? "0" + i : i);
